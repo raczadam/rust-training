b1d3f0743b44638cadb18f6f7752346767dd1bb7
Rácz Ádám <racz.adam.istvan@gmail.com>
2024-04-16 21:40:42 +0200

    create project variables, and add some numeric types.

---
+++ variables/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    let x = 6;
+    println!("The value of x is {}", x);
+    // x = 5; => it will not work, we cannot assign twice to immutable variable `x`
+
+    // but we can do this:
+    let x = "IAmAStringNow"; // shadowing
+    println!("The value of x is {}", x);
+
+    let mut y = 5;
+    println!("The value of y is {}", y);
+    y = 6;
+    println!("The new value of y is {}", y);
+
+    const MEANING_OF_LIFE: u32 = 42;
+    println!("The secret meaning of life is {}", MEANING_OF_LIFE);
+
+    let default_integer = 23; // i32 is the default integer type
+    println!("default_integer value is: {}", default_integer);
+
+    let default_integer = 0xABCDEF; // it can be hexadecimal
+    println!("default_integer (hexa) value is: {}", default_integer);
+
+    let default_integer = 0o777; // or octal
+    println!("default_integer (octal) value is: {}", default_integer);
+
+    let default_integer = 0b010101010001010101011010100101; // or binary
+    println!("default_integer (binary) value is: {}", default_integer);    
+
+    let one_byte: u8 = 255; // unsigned: can't be negative
+    println!("byte value is: {}", one_byte);
+
+    let large_signed_integer: i128 = -1_000_000_000_000_000_000_000_000_000_000_000; // signed: can be negative
+    println!("byte value is: {}", large_signed_integer);
+
+    let x = 2.0; // f64 is the default floating point type
+    println!("The value of x is {}", x);
+
+    let y: f32 = 3.0; // f32
+    println!("The value of y is {}", y);
+    
+}
