345adcffe64f717d5eff2d5aa9118343da5dffd6
raczadam <racz.adam.istvan@gmail.com>
2024-12-11 08:11:05 +0100

    Use new dependencies in hello project, to ensure that java can use palindrome method.

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,16 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use jni::objects::JString;
+use jni::sys::jboolean;
+use jni::JNIEnv;
 
-/*
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[no_mangle]
+pub extern "system" fn Java_org_example_App_palindrome(
+    env: JNIEnv,
+    _class: jni::objects::JClass,
+    input: JString,
+) -> jboolean {
+    let input: String = match env.get_string(input) {
+        Ok(s) => s.into(),
+        Err(_) => return 0,
+    };
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    if palindrome(input) {
+        return 1;
     }
+    return 0;
 }
-*/
+
+fn palindrome(input: String) -> bool {
+    if input.len() == 0 {
+        return true;
+    }
+    let mut last = input.len() - 1;
+    let mut first = 0;
+
+    let my_vec = input.as_bytes();
+
+    while first < last {
+        if my_vec[first] != my_vec[last] {
+            return false;
+        }
+        first += 1;
+        last -= 1;
+    }
+    return true;
+}
